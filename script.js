e4e35c420468c6f3d78b69b62096940349a01aa3
abduzeedo <fabio.sasso@gmail.com>
2024-01-13 14:45:13 -0800

    Redesign
    New html, css, and js

---
+++ script.js
@@ -0,0 +1,52 @@
+const assets = document.querySelectorAll("img");
+let loadedAssets = 0;
+
+assets.forEach((asset) => {
+  asset.addEventListener("load", () => {
+    loadedAssets++;
+    updateProgressBar(loadedAssets, assets.length);
+  });
+});
+function updateProgressBar(loaded, total) {
+  const progress = (loaded / total) * 100;
+  document.getElementById("progressBar").style.width = progress + "%";
+  document.getElementById("progressPercentage").innerText =
+    Math.round(progress) + "%";
+
+  if (loaded === total) {
+    document.getElementById("progressBarContainer").style.display = "none";
+    document.querySelector(".grid").style.opacity = "100";
+  }
+}
+function expand_collapse() {
+  var item = ""
+  document.querySelectorAll("ol li a").forEach((item) => {
+    item.addEventListener("click", function (event) {
+      // Prevent default action if needed
+      event.preventDefault();
+      // Toggle 'expanded' class on the parent <li> element
+      
+      if(!item.parentElement.classList.contains("expanded")){
+        clearExpansion()
+      }
+      item.parentElement.classList.toggle("expanded");
+      
+    });
+  });
+}
+function clearExpansion(){
+    // Remove from previous
+    document.querySelectorAll("ol li").forEach((item) => {
+        item.classList.remove("expanded");
+    })
+}
+window.onload = function () {
+  // This will hide the progress bar and show the content
+  // once everything on the page has loaded
+  document.getElementById("progressBarContainer").style.display = "none";
+  document.querySelector(".grid").style.opacity = "100";
+};
+document.addEventListener("DOMContentLoaded", function () {
+  // Adjust the interval time as needed
+  expand_collapse()
+});
